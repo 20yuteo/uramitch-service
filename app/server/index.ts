3a7630e8d7c505d2da77b6d9ed654053df8fa3de
20yuteo <20yuteo@gmail.com>
2025-02-04 20:59:25 +0900

    Disable static file serving in Hono server configuration

--- app/server/index.ts
+++ app/server/index.ts
@@ -1,12 +1,12 @@
 /* eslint-disable import/no-unresolved */
 import { serve } from "@hono/node-server";
-import { serveStatic } from "@hono/node-server/serve-static";
+// import { serveStatic } from "@hono/node-server/serve-static";
 import { Hono } from "hono";
 
 const app = new Hono();
 
 // 静的ファイルの配信
-app.use("/*", serveStatic({ root: "./build/client" }));
+// app.use("/*", serveStatic({ root: "./build/client" }));
 
 // ヘルスチェックエンドポイント
 app.get("/api/v1/health", (c) => {
